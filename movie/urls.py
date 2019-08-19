4115bef50539cb87d6d79a4b1d88f7bf2b480db7
Aman Kumar <meamank@gmail.com>
2019-08-19 14:49:51 +0530

    intial commit

---
+++ movie/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from .views import MovieList , MovieDetail , MovieCategory, MovieLanguage , MovieSearch , MovieYear
+
+app_name = 'movie'
+
+urlpatterns = [
+    path('', MovieList.as_view() , name='movie_list'),
+    path('category/<str:category>', MovieCategory.as_view() , name='movie_category'),
+    path('language/<str:language>', MovieLanguage.as_view() , name='movie_language'),
+    path('search/', MovieSearch.as_view() , name='movie_search'),
+    path('year/<int:year>', MovieYear.as_view() , name='movie_year'),
+    path('<slug:slug>', MovieDetail.as_view() , name='movie_detail'),
+]
+
---
+++ outline.md
@@ -0,0 +1,28 @@
+- movie :
+	- title
+	- description
+	- tags
+	- views count
+	- image
+	- category
+	- language
+	- year if production
+	- status [recently added - most watched - top rated]
+	- download links
+	- watch links
+
+	- related movies
+	- cast
+	- trailer_link
+
+-  search
+
+- No movies in year
+- slug
+- movie trailer
+- slider
+- related movies
+- tags
+- home
+
+	
